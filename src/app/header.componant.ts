2dcb2adc1900056545ddee73decd1abc5e7012bf
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-01 18:42:31 +0300

    uploading img

--- src/app/header.componant.ts
+++ src/app/header.componant.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-header', //which element should be replaced by our componant
-  standalone:true, //configuration property , its module-base when its false
-  templateUrl: './header.componant.html' , //defines the markeup(html content) that should be displayed by that componant
-  styleUrl: './header.componant.css'
+  standalone: true, //configuration property , its module-base when its false
+  templateUrl: './header.componant.html', //defines the markeup(html content) that should be displayed by that componant
+  styleUrl: './header.componant.css',
 })
 export class HeaderComponant {}
