73ece30ad448e22425dab1f2ec29cfde4ddb9f07
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-02 13:34:09 +0300

    using computed property with signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component , signal} from '@angular/core';
+import { Component , computed, signal} from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -16,11 +16,13 @@ export class UserComponent {
 
   //add users dynamically
   selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   //method inside the class
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+
   //add method to call on event listener
   onSelectUser(){
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
