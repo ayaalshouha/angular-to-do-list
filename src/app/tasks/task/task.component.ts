74cc91f5b148a0faaecec9e2d417e8e1d49e5df1
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-04 21:34:04 +0300

    improvements

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { RequiredValidator } from '@angular/forms';
 import { type Task } from './task.model';
 @Component({
   selector: 'app-task',
