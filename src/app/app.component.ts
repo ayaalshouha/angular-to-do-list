b7da99c66748b7456d11962e342d06c6ce5ba860
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-04 18:39:56 +0300

    if condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserID = 'u1';
+  selectedUserID?: string;
 
   get selectedUser() {
     return this.users.find((e) => e.id === this.selectedUserID);
