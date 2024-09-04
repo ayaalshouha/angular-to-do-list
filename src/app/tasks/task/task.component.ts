a3c25107724ea206164b3b2b0e3222f85bc7c368
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-04 21:11:20 +0300

    manage task html

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) title!: string;
+  @Input({ required: true }) time!: string;
+  @Input({ required: true }) summary!: string;
 }
