4ec579de07abe5795964a9dc5ccad864ee9b5f6d
ayaalshouha <ayaalshouha12@gmail.com>
2024-09-07 21:06:42 +0300

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponant } from './header/header.componant';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponant, UserComponent, [TasksComponent]],
+  imports: [HeaderComponant, UserComponent, TasksComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
